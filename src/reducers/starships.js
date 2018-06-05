00975b529ece013d555fe3a8ac75b389c1a9eeca
FStriewski <fstriewski@outlook.com>
2018-06-05 14:30:09 +0200

    Comments

--- src/reducers/starships.js
+++ src/reducers/starships.js
@@ -1,6 +1,6 @@
 import FETCH_ALL_STARSHIPS from '../actions/starships'
 
-export default function (starship = {}, action = {}) {
+export default function (state = {}, action = {}) {
 
     switch (action.type) {
         case 'FETCH_ALL_STARSHIPS':
@@ -9,20 +9,21 @@ export default function (starship = {}, action = {}) {
 
             for (let starship of action.payload.results) {
                 _starships = {
+                    // Appending the object with the property from iteration:
                     ..._starships,
+                    // I don't have an id, so name. This will store the ship under its name.
                     [starship.name]: starship
                 }
             }
 
             
             return {
-                ...starship,
+                // Append this to the state we had before:
+                ...state,
                 ..._starships
             }
-           
-           // return action.payload
 
         default:
-            return starship
+            return state
     }
 }
